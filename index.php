2300719ff93caa44731fc7361298cf73fa95ce4d
Replit user <>
2023-08-09 05:25:46 +0000

    updates styles

--- index.php
+++ index.php
@@ -8,56 +8,74 @@
       @import url('https://fonts.googleapis.com/css2?family=Tulpen+One&display=swap');
       @import url('https://fonts.googleapis.com/css2?family=Qwitcher+Grypen:wght@700&display=swap');
 
-      /* Styling for quote container and animation */
-      #quoteContainer {
-        display: none;
-        text-shadow: 4px 4px 4px #aaa;
-      }
-
+      /* Styling for header and body */
       body {
-        background-color: #f5f5dc;
-        font-family: Arial, sans-serif;
+        margin: 0;
+        padding: 0;
+        background-color: #f0f0f0;
+        font-family: 'Arial', sans-serif;
+        color: #333;
       }
 
-      .fade-in {
-        animation: fadeIn 1s ease;
+      /* Header styling */
+      header {
+        background-color: #3498db;
+        text-align: center;
+        padding: 1rem;
       }
 
-      @keyframes fadeIn {
-        from { opacity: 0; }
-        to { opacity: 1; }
-      }
-
-      /* Additional styling for the header and quote */
       h1 {
-        color: #8e44ad; /* Change header color */
-        font-family: 'Shadows Into Light', cursive; /* Change header font */
+        margin: 0;
+        font-family: 'Shadows Into Light', cursive;
+        color: #fff;
+      }
+
+      /* Container styling */
+      .container {
+        text-align: center;
+        padding: 2rem;
+        background-color: #fff;
+        box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+        border-radius: 10px;
+        max-width: 600px;
+        margin: 0 auto;
       }
 
       /* Styling for the quote itself */
       #quoteContainer {
-        /* ... (existing styles) ... */
-        color: #34495e; /* Change quote color */
+        display: none;
+        text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+        font-size: 1.5rem;
+        margin-top: 1.5rem;
       }
 
       /* Styling for paragraphs */
       p {
+        font-size: 1.2rem;
+        line-height: 1.5;
         color: #666;
       }
     </style>
   </head>
   <body>
-    <h1>Welcome to AJAX Quotes</h1>
-    <p>Enjoy a new inspiring quote every 5 seconds!</p>
-    <div id="quoteContainer">Quote goes here</div>
-    <p>This page retrieves random quotes from a PHP server using AJAX. A quote is displayed on page load and updates every few seconds.</p>
+    <header>
+      <h1>AJAX Quotes</h1>
+    </header>
+    <div class="container">
+      <p>Get inspired with a new quote every 5 seconds!</p>
+      <div id="quoteContainer">Quote goes here</div>
+      <p>This page retrieves random quotes from a server using AJAX. A quote is displayed on page load and updates every few seconds.</p>
+    </div>
     
     <script>
       var counter = 0;
       var quotes = [
         "The only way to do great work is to love what you do. - Steve Jobs",
         "In the middle of every difficulty lies opportunity. - Albert Einstein",
-        "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill"
+        "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+        "The journey of a thousand miles begins with one step. - Lao Tzu",
+        "Believe you can and you're halfway there. - Theodore Roosevelt",
+        "Don't watch the clock; do what it does. Keep going. - Sam Levenson"
       ];
 
       function getRandomQuote() {
@@ -73,11 +91,7 @@
             quoteContainer.innerText = xhr.responseText;
             quoteContainer.style.display = 'block';
             quoteContainer.style.fontFamily = fonts[counter];
-            counter++;
-            
-            if (counter >= fonts.length) {
-              counter = 0;
-            }
+            counter = (counter + 1) % fonts.length; // Loop through fonts
             
             quoteContainer.classList.add("fade-in");
 
